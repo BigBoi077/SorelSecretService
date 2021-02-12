f21abe01239780fd14f441f6f06fb3ec6131f10b
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-02-11 21:56:43 -0500

    Initial commit

---
+++ app/src/main/java/cegepst/example/sorelsecretservice/MainActivity.kt
@@ -0,0 +1,19 @@
+package cegepst.example.sorelsecretservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initMenu()
+    }
+
+    private fun initMenu() {
+
+    }
+}
