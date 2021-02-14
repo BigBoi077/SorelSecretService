e1e61aa1fa6d6affb93818539e2e7114797e506b
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-02-14 12:50:54 -0500

    Made the menu functional, added background and activity titles

--- app/src/main/java/cegepst/example/sorelsecretservice/views/MainActivity.kt
+++ app/src/main/java/cegepst/example/sorelsecretservice/views/MainActivity.kt
@@ -1,8 +1,10 @@
 package cegepst.example.sorelsecretservice.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import cegepst.example.sorelsecretservice.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initMenu() {
-
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation_menu)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.addSuspiciousActivityMenuButton -> {
+                    val intent = Intent(this, AddSuspiciousActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.suspiciousActivitiesMenuButton -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.suspiciousActivitiesStatsMenuButton -> {
+                    val intent = Intent(this, StatsActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            true
+        }
     }
 }
