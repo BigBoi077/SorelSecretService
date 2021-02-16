c783876598ad66131e2fe1ae16f4fad981a59328
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-02-16 11:25:04 -0500

    Made the database "manager"

---
+++ app/src/main/java/cegepst/example/sorelsecretservice/stores/AppStore.kt
@@ -0,0 +1,33 @@
+package cegepst.example.sorelsecretservice.stores
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import cegepst.example.sorelsecretservice.dao.BehaviourDAO
+import cegepst.example.sorelsecretservice.dao.SuspiciousActivityDAO
+import cegepst.example.sorelsecretservice.models.SuspiciousActivity
+
+@Database(entities = [SuspiciousActivity::class], version = 1)
+abstract class AppStore : RoomDatabase() {
+
+    abstract fun suspiciousActivitiesDAO(): SuspiciousActivityDAO
+    abstract fun behaviorsDAO(): BehaviourDAO
+
+    companion object {
+        @Volatile
+        private var instance: AppStore? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context,
+            AppStore::class.java,
+            "Sorel-Secret-Service"
+        )
+            .build()
+    }
+}
