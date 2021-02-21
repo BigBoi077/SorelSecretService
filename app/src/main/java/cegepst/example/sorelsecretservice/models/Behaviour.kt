c91b8406c4ebe710fd7a35b08d9dbdee226728fe
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2021-02-21 12:12:02 -0500

    Changed models to add the primary key

--- app/src/main/java/cegepst/example/sorelsecretservice/models/Behaviour.kt
+++ app/src/main/java/cegepst/example/sorelsecretservice/models/Behaviour.kt
@@ -2,10 +2,14 @@ package cegepst.example.sorelsecretservice.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "behavior")
 data class Behaviour(
-    @ColumnInfo(name = "id_behavior") val ID: Long,
     @ColumnInfo(name = "beahvior_name") var name: String,
     @ColumnInfo(name = "behavior_description") var description: String
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var ID: Long = 0
+
+}
--- app/src/main/java/cegepst/example/sorelsecretservice/models/SuspiciousActivity.kt
+++ app/src/main/java/cegepst/example/sorelsecretservice/models/SuspiciousActivity.kt
@@ -2,12 +2,15 @@ package cegepst.example.sorelsecretservice.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "suspicious_activity")
 class SuspiciousActivity(
-    @ColumnInfo(name = "id_activity") val ID: Long,
     @ColumnInfo(name = "trust_level") val trustLevel: Int,
     @ColumnInfo(name = "behavior_id") val behaviorID: Long,
     @ColumnInfo(name = "location") val location: String,
     @ColumnInfo(name = "date_creation") val createdDate: Long
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var ID: Long = 0
+}
